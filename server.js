ede727e4fb0908d09cbc5156b755c72e5e81fbce
augastian <30323058+augastian@users.noreply.github.com>
2018-07-02 15:07:10 +0530

    hhh

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var knex = require('knex')
 const db= knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl : true,
   }
 });
